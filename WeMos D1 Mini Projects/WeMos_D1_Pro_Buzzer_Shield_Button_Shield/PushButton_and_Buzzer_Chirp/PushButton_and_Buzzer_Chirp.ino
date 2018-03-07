b7ae37f167fc7e88a47a80507a3ae421c34d0549
kimmo.fonsell <kimmo.fonsell@mediconsult.fi>
2018-03-07 09:56:30 +0200

    play  Star Wars with buzzer

--- WeMos D1 Mini Projects/WeMos_D1_Pro_Buzzer_Shield_Button_Shield/PushButton_and_Buzzer_Chirp/PushButton_and_Buzzer_Chirp.ino
+++ WeMos D1 Mini Projects/WeMos_D1_Pro_Buzzer_Shield_Button_Shield/PushButton_and_Buzzer_Chirp/PushButton_and_Buzzer_Chirp.ino
@@ -14,6 +14,36 @@ void setup() {
   digitalWrite(ledPin, buttonState);
 }
 
+void beep (int speakerPin, float noteFrequency, long noteDuration){
+  int x;
+  // Convert the frequency to microseconds
+  float microsecondsPerWave = 1000000/noteFrequency;
+  // Calculate how many milliseconds there are per HIGH/LOW cycles.
+  float millisecondsPerCycle = 1000/(microsecondsPerWave * 2);
+  // Multiply noteDuration * number or cycles per millisecond
+  float loopTime = noteDuration * millisecondsPerCycle;
+  // Play the note for the calculated loopTime.
+  for (x=0;x<loopTime;x++)
+  {
+  digitalWrite(speakerPin,HIGH);
+  delayMicroseconds(microsecondsPerWave);
+  digitalWrite(speakerPin,LOW);
+  delayMicroseconds(microsecondsPerWave);
+  }
+}
+
+void ohhh() {
+  for (int i=1000; i<2000; i=i*1.02) { beep(buzzer,i,10); } for (int i=2000; i>1000; i=i*.98) {
+  beep(buzzer,i,10);
+  }
+}
+
+void uhoh() {
+  for (int i=1000; i<1244; i=i*1.01) { beep(buzzer,i,30); } delay(200); for (int i=1244; i>1108; i=i*.99) {
+  beep(buzzer,i,30);
+ }
+}
+
 void tone(uint8_t _pin, unsigned int frequency, unsigned long duration, unsigned long pause) {
   pinMode (_pin, OUTPUT );
   analogWriteFreq(frequency);
@@ -25,9 +55,109 @@ void tone(uint8_t _pin, unsigned int frequency, unsigned long duration, unsigned
 
 void cricket(){
   for(int i=0; i<4; i++){
-    tone(buzzer, 250, 20, 15);
+    tone(buzzer, 50, 20, 15);
   }
-  delay(450);
+  delay(50);
+}
+
+void musicSection1(){
+  //delay(500);
+  //Star Wars
+  //1
+  tone(buzzer, 440, 500, 150);
+  tone(buzzer, 440, 500, 150);
+  tone(buzzer, 440, 500, 150);
+
+  tone(buzzer, 349, 350, 80);
+  tone(buzzer, 523, 150, 80);
+  tone(buzzer, 440, 500, 100);
+
+  tone(buzzer, 349, 350, 80);
+  tone(buzzer, 523, 150, 80);
+  tone(buzzer, 440, 650, 100);
+
+  delay(500);
+  //2
+  tone(buzzer, 659, 500, 150);
+  tone(buzzer, 659, 500, 150);
+  tone(buzzer, 659, 500, 150);
+
+  tone(buzzer, 698, 350, 80);
+  tone(buzzer, 523, 150, 80);
+  tone(buzzer, 415, 500, 100);
+
+  tone(buzzer, 349, 350, 80);
+  tone(buzzer, 523, 150, 80);
+  tone(buzzer, 440, 650, 100);
+
+  delay(500);
+}
+
+void musicSection2(){
+  //delay(500);
+  //Star Wars
+  //1
+  tone(buzzer, 880, 500, 150);
+  tone(buzzer, 440, 300, 150);
+  tone(buzzer, 440, 150, 150);
+
+  tone(buzzer, 880, 500, 80);
+  tone(buzzer, 830, 325, 80);
+  tone(buzzer, 784, 175, 100);
+
+  tone(buzzer, 740, 125, 80);
+  tone(buzzer, 698, 125, 80);
+  tone(buzzer, 740, 250, 100);
+
+  delay(325);
+  //2
+  tone(buzzer, 466, 250, 150);
+  tone(buzzer, 622, 500, 150);
+  tone(buzzer, 587, 325, 150);
+
+  tone(buzzer, 554, 175, 80);
+  tone(buzzer, 523, 125, 80);
+  tone(buzzer, 494, 125, 100);
+
+  tone(buzzer, 523, 250, 80);
+
+  delay(350);
+}
+
+void musicVariant1(){
+  //delay(500);
+  //Star Wars
+  //1
+  tone(buzzer, 349, 250, 150);
+  tone(buzzer, 415, 500, 150);
+  tone(buzzer, 349, 350, 150);
+
+  tone(buzzer, 440, 125, 80);
+  tone(buzzer, 523, 500, 80);
+  tone(buzzer, 440, 375, 100);
+
+  tone(buzzer, 523, 125, 80);
+  tone(buzzer, 659, 650, 80);
+
+  delay(500);
+}
+
+void musicVariant2(){
+  //delay(500);
+  //Star Wars
+  //1
+  tone(buzzer, 349, 250, 150);
+  tone(buzzer, 415, 500, 150);
+  tone(buzzer, 349, 375, 150);
+
+  tone(buzzer, 523, 125, 80);
+  tone(buzzer, 440, 500, 80);
+  tone(buzzer, 349, 375, 100);
+
+  tone(buzzer, 523, 125, 80);
+  tone(buzzer, 440, 650, 80);
+
+  delay(650);
 }
 
 void loop() {
@@ -39,6 +169,14 @@ void loop() {
     digitalWrite(ledPin, HIGH);  // LED on
   } else {
     digitalWrite(ledPin, LOW); // LED off
-    cricket();
+    //cricket();
+    // Star Wars
+    musicSection1();
+    musicSection2();
+    musicVariant1();
+    musicSection2();
+    musicVariant2();
+    //ohhh();
+    //uhoh();
   }
 }
